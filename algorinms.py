3b5c1f1d05a9f48b4fc096b45574bd74884ff650
KolBbl4 <ko1ba@mail.ru>
2024-07-24 16:36:07 +0500

    Добавли алгоритм бинарного поиска

--- algorinms.py
+++ algorinms.py
@@ -8,5 +8,29 @@ def boobleSorts (ms):
         n += 1
     return ms
 #Test
+
+
+
+def BinarySearch (ms,searchInt):
+    low = 0;
+    hight = len(ms) - 1
+
+    for i in range (len(ms)):
+        middle = (low + hight)//2
+        ser = ms[middle]
+        if ser == searchInt:
+            return True;
+        if ser > searchInt:
+            hight = middle - 1;
+        else:
+            low = middle + 1;
+
+    return False;
+
 ms = [3,4,1,2,6]
-print(boobleSorts(ms))
+sortMs = boobleSorts(ms)
+resul = BinarySearch(sortMs,5)
+if resul:
+    print('Есть такое число')
+else:
+    print('НЕТ')
